be73be826c0dc192f417fbf7a92588ccf9a3497d
Jonathan Araul <jonathan.araul@gmail.com>
2013-09-17 16:48:04 -0430

    Update 17/09/2013

--- src/Proyecto/FrontBundle/Controller/DefaultController.php
+++ src/Proyecto/FrontBundle/Controller/DefaultController.php
@@ -8,13 +8,49 @@ use Proyecto\PrincipalBundle\Entity\Categoria;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function inicioAction()
     {
-		$name = '';
+		$array['menu'] = 'inicio';
 		
-	
+        return $this->render('ProyectoFrontBundle:Default:inicio.html.twig', $array );
+    }
+    public function organizacionAction()
+    {
+		$array['menu'] = 'organizacion';
 		
+        return $this->render('ProyectoFrontBundle:Default:organizacion.html.twig', $array );
+    }
+    public function descargasAction()
+    {
+		$array['menu'] = 'descargas';
+		
+		$array['objects'] = $this->getDoctrine()
+        ->getRepository('ProyectoPrincipalBundle:Categoria')
+        ->findByTipo(1);
+		
+		
+		$em = $this->getDoctrine()->getManager();
+		$query = $em -> createQuery('SELECT d
+    								 FROM ProyectoPrincipalBundle:Data d,
+    	 								  ProyectoPrincipalBundle:Categoria c
+   	 								 WHERE d.categoria = c.id AND
+   	 								       c.tipo = :tipo
+    								 ORDER BY d.fecha DESC') -> setParameter('tipo', '1');
 
-        return $this->render('ProyectoFrontBundle:Default:index.html.twig', array('name' => $name));
+		$array['downloads'] = $query -> getResult();
+
+        return $this->render('ProyectoFrontBundle:Default:descargas.html.twig', $array );
+    }
+    public function contactoAction()
+    {
+		$array['menu'] = 'contacto';
+		
+        return $this->render('ProyectoFrontBundle:Default:contacto.html.twig', $array );
+    }
+    public function informacionAction()
+    {
+		$array['menu'] = 'informacion';
+		
+        return $this->render('ProyectoFrontBundle:Default:informacion.html.twig', $array );
     }
 }
