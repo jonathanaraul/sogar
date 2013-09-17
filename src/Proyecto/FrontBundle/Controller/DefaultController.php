61c0067bda2d18402157ca2f2aa2940694b298bb
Jonathan Araul <jonathan.araul@gmail.com>
2013-09-17 00:10:28 -0430

    Update 12:10am

--- src/Proyecto/FrontBundle/Controller/DefaultController.php
+++ src/Proyecto/FrontBundle/Controller/DefaultController.php
@@ -3,13 +3,18 @@
 namespace Proyecto\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Proyecto\PrincipalBundle\Entity\Data;
+use Proyecto\PrincipalBundle\Entity\Categoria;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-    	echo 'Hola estas en el front ;-) ';
-		exit;
+		$name = '';
+		
+	
+		
+
         return $this->render('ProyectoFrontBundle:Default:index.html.twig', array('name' => $name));
     }
 }
